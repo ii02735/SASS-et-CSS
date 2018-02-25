fd3d023dcf66c5a12cff6894fbb6406397be0036
YadalleeBilaal <muhammad.yadallee@etu.paridescartes.fr>
2018-02-25 17:01:17 +0100

    Projet vierge

---
+++ rubyinstaller-2.5.0-1-x86/bin/sass.bat
@@ -0,0 +1,6 @@
+@ECHO OFF
+IF NOT "%~f0" == "~f0" GOTO :WinNT
+@"C:\Users\bilaa\Documents\sass\rubyinstaller-2.5.0-1-x86\bin\ruby.exe" "C:/Users/bilaa/Documents/sass/rubyinstaller-2.5.0-1-x86/bin/sass" %1 %2 %3 %4 %5 %6 %7 %8 %9
+GOTO :EOF
+:WinNT
+@"C:\Users\bilaa\Documents\sass\rubyinstaller-2.5.0-1-x86\bin\ruby.exe" "%~dpn0" %*
---
+++ rubyinstaller-2.5.0-1-x86/lib/ruby/site_ruby/2.5.0/ruby_installer/runtime/package_version.rb
@@ -0,0 +1,6 @@
+module RubyInstaller
+module Runtime
+PACKAGE_VERSION = "2.5.0-1"
+GIT_COMMIT = "46edcb7"
+end
+end
